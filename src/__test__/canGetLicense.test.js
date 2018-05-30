865bafc936a9c7837068071fea2ede1990163564
sara <saraa379@yahoo.com>
2018-05-30 18:50:55 +0200

    Körkort test is done

--- src/__test__/canGetLicense.test.js
+++ src/__test__/canGetLicense.test.js
@@ -1,14 +1,91 @@
 import {canGetLicense} from '../canGetLicense.js';
 
 describe('canGetLicense test suite', () => {
-		test('red test', () => {
+		test('age higher than boundary', () => {
 				let license = 'A';
-				let age = 20;
+				let age = 121;
+				expect(() => canGetLicense({})).toThrow();
+		});
+		test('age less than boundary', () => {
+				let license = 'A';
+				let age = -1;
+				expect(() => canGetLicense({})).toThrow();
+		});
+		test('age is not a number', () => {
+				let license = 'A';
+				let age = "kalle";
+				expect(() => canGetLicense({})).toThrow();
+		});
+		test('valid input for A license', () => {
+				let license = 'A';
+				let age = 24;
 				let actual = canGetLicense(license, age);
-				let expected = "A";
+				let expected = true;
 				expect(actual).toBe(expected);
 		});
-
+		test('Underage for A license', () => {
+				let license = 'A';
+				let age = 23;
+				let actual = canGetLicense(license, age);
+				let expected = false;
+				expect(actual).toBe(expected);
+		});
+		test('valid input for B license', () => {
+				let license = 'B';
+				let age = 18;
+				let actual = canGetLicense(license, age);
+				let expected = true;
+				expect(actual).toBe(expected);
+		});
+		test('Underage for B license', () => {
+				let license = 'B';
+				let age = 17;
+				let actual = canGetLicense(license, age);
+				let expected = false;
+				expect(actual).toBe(expected);
+		});
+		test('valid input for BE license', () => {
+				let license = 'BE';
+				let age = 18;
+				let actual = canGetLicense(license, age);
+				let expected = true;
+				expect(actual).toBe(expected);
+		});
+		test('Underage for BE license', () => {
+				let license = 'BE';
+				let age = 17;
+				let actual = canGetLicense(license, age);
+				let expected = false;
+				expect(actual).toBe(expected);
+		});
+		test('valid input for C license', () => {
+				let license = 'C';
+				let age = 21;
+				let actual = canGetLicense(license, age);
+				let expected = true;
+				expect(actual).toBe(expected);
+		});
+		test('Underage for C license', () => {
+				let license = 'C';
+				let age = 20;
+				let actual = canGetLicense(license, age);
+				let expected = false;
+				expect(actual).toBe(expected);
+		});
+		test('valid input for D license', () => {
+				let license = 'D';
+				let age = 24;
+				let actual = canGetLicense(license, age);
+				let expected = true;
+				expect(actual).toBe(expected);
+		});
+		test('Underage for D license', () => {
+				let license = 'D';
+				let age = 23;
+				let actual = canGetLicense(license, age);
+				let expected = false;
+				expect(actual).toBe(expected);
+		});
 			test('throws if parameters are not valid', () => {
 			expect(() => canGetLicense({})).toThrow();
 		})
--- src/canGetLicense.js
+++ src/canGetLicense.js
@@ -1,23 +1,42 @@
 
 function canGetLicense(license, age) {
-	switch(license) {
-				case "A":
-						return license;
-						break;
-				case "B":
-				case "BE":
-						return license;
-						break;
-				case "C":
-						return license;
-						break;
-				case "D":
-						return license;
-						break;
-				default:
-						throw new Error('Invalid license');
-						return false;
-	}//end of switch
+	var typeAge = typeof(age);
+	if(typeAge !== "number") {
+			throw new Error('Invalid age type');
+			console.log("Age is invalid");
+	} else if (age < 0 || age > 120){
+			throw new Error('Invalid age boundary');
+			console.log("Age is invalid");
+	} else {
+			switch(license) {
+						case "A":
+						case "D":
+								if(age >= 24){
+										return true;
+								} else {
+										return false;
+								}
+								break;
+						case "B":
+						case "BE":
+								if(age >= 18){
+										return true;
+								} else {
+										return false;
+								}
+								break;
+						case "C":
+								if(age >= 21){
+										return true;
+								} else {
+										return false;
+								}
+								break;
+						default:
+								throw new Error('Invalid license');
+								console.log("License is invalid");
+			}//end of switch
+		}//end if else
 
 }//end of canGetLicense
 
